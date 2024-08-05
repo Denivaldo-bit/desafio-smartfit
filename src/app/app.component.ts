248cf7370e9679f92f97088d29725830def17092
denivaldo-bit <ferreiradenivaldo9@gmail.com>
2024-08-05 19:25:03 -0300

    feat: create form structure and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsComponent } from './components/forms/forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-imports: [CommonModule, RouterModule, HeaderComponent],
+imports: [CommonModule, RouterModule, HeaderComponent, FormsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'desafio-smartfit';
