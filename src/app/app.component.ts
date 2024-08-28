067b7ff5428b69dcb70a529476857d1bb716b93b
denivaldo-bit <ferreiradenivaldo9@gmail.com>
2024-08-28 07:10:40 -0300

    feat: add form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsComponent } from './components/forms/forms.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-imports: [CommonModule, RouterModule, HeaderComponent, FormsComponent],
+imports: [CommonModule, RouterModule, HeaderComponent, FormsComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from '../header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [FormsComponent, ReactiveFormsModule, HeaderComponent, CommonModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
+
+export class FormsComponent implements OnInit {
 
   results = [];
+  formGroup!: FormGroup;
 
+
+  constructor(private formBuilder: FormBuilder) {}
+
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group ({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value)
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
 }
